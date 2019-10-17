d2987d178db43707cf19821c7701d2c77e17319f
Patrick McGovern <patrick.mcgovern@worldpay.com>
2019-10-17 14:30:52 +0200

    Update member with given id to 'confirmed'

--- routes/update.js
+++ routes/update.js
@@ -1,9 +1,29 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.json({ title: 'Update attempted' });
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'keyZ0TTQdstBfuP3H'}).base('appT1QHGIE3H9c5Dn');
+
+router.get('/:memberId', function (request, response) {
+    base('Members').update([
+        {
+            "id": request.param('memberId'),
+            "fields": {
+                "Contact type": "Confirmed"
+            }
+        }
+    ], function (err, records) {
+        if (err) {
+            console.error(err);
+            response.json({title: 'Update failed'});
+            return;
+        }
+        records.forEach(function (record) {
+            let name = record.get('Name');
+            console.log(name);
+            response.json({title: "Updated " + name});
+        });
+    });
 });
 
 module.exports = router;
