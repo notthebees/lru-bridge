1f9eae0a3f767e230e5c80e5d127c8d9970f1335
Patrick McGovern <patrick.mcgovern@worldpay.com>
2019-10-17 21:12:15 +0200

    Update Airtable based on GoCardless info

--- routes/getMember.js
+++ routes/getMember.js
@@ -1,12 +1,47 @@
 const express = require('express');
 const router = express.Router();
 const request = require('request');
+const Airtable = require('airtable');
+const base = new Airtable({apiKey: 'keyZ0TTQdstBfuP3H'}).base('appT1QHGIE3H9c5Dn');
+
+function updateMember(memberId) {
+    base('Members').update([
+        {
+            "id": memberId,
+            "fields": {
+                "Contact type": "Confirmed"
+            }
+        }
+    ], function (err, records) {
+        if (err) {
+            console.error(err);
+            return;
+        }
+        records.forEach(function (record) {
+            let name = record.get('Name');
+            console.log(name);
+        });
+    });
+}
 
 function callback3(error, response, body) {
     if (!error && response.statusCode === 200) {
         const info = JSON.parse(body);
         const email = info.customers.email;
         console.log(email);
+
+        base('Members').select({
+            maxRecords: 1,
+            view: 'Grid view',
+            filterByFormula: "{Email address} = '" + email + "'"
+        }).firstPage(function(err, records) {
+            if (err) { console.error(err); return; }
+            records.forEach(function(record) {
+                var memberId = record.id;
+                console.log('Retrieved', memberId);
+                updateMember(memberId);
+            });
+        });
     }
 }
 
