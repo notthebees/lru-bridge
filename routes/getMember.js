7e65fae3d9ff4be46fd596a2a158e139651b85b2
Patrick McGovern <patrick.mcgovern@worldpay.com>
2019-10-17 20:44:23 +0200

    Retrieve specific customer

--- routes/getMember.js
+++ routes/getMember.js
@@ -1,12 +1,31 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const request = require('request');
 
-
-function callback(error, response, body) {
+function callback2(error, response, body) {
     if (!error && response.statusCode === 200) {
         const info = JSON.parse(body);
-        console.log(info.subscriptions.links.mandate);
+        const customerId = info.mandates.links.customer;
+        console.log(customerId);
+    }
+}
+
+function callback1(error, response, body) {
+    if (!error && response.statusCode === 200) {
+        const info = JSON.parse(body);
+        const mandateId = info.subscriptions.links.mandate;
+        console.log(mandateId);
+
+        const options = {
+            url: 'https://api-sandbox.gocardless.com/mandates/' + mandateId,
+            headers: {
+                'Accept': 'application/json',
+                'Authorization': 'Bearer sandbox_cCnuVtVVOvn9sl12SkoXt2M2zPsby7szcH77LDYi',
+                'GoCardless-Version': '2015-07-06'
+            }
+        };
+
+        request(options, callback2)
     }
 }
 
@@ -20,12 +39,12 @@ function getMandate(subscriptionId) {
         }
     };
 
-    request(options, callback);
+    request(options, callback1);
 }
 
 router.get('/', function (request, response) {
     getMandate('SB0001RTC070X7');
-    response.json({title: 'Subscription retrieved'});
+    response.json({title: 'Customer retrieved'});
 });
 
 module.exports = router;
