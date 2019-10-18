d5d7b37d28a7dd1a8da40584c82def9f8f9fc5d5
Patrick McGovern <patrick.mcgovern@worldpay.com>
2019-10-18 23:13:59 +0200

    Mock GoCardless requests

--- test/updateMemberTest.js
+++ test/updateMemberTest.js
@@ -3,19 +3,52 @@ const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
 const should = chai.should();
+const nock = require('nock');
+
+const subscriptionId = "someSubscriptionId";
+const mandateId = "someMandateId";
+const customerId = "someCustomerId";
+
+const webhook = {
+    events: [
+        {
+            links: {
+                subscription: subscriptionId
+            }
+        }
+    ]
+};
 
 describe('updateMember', function () {
-    it('should return a 204', function () {
-        const webhook = {
-            events: [
-                {
+    beforeEach(() => {
+        nock('https://api-sandbox.gocardless.com')
+            .get('/subscriptions/' + subscriptionId)
+            .reply(200, {
+                subscriptions: {
                     links: {
-                        subscription: "SB0001RTC070X7"
+                        mandate: mandateId
                     }
                 }
-            ]
-        };
+            });
+        nock('https://api-sandbox.gocardless.com')
+            .get('/mandates/' + mandateId)
+            .reply(200, {
+                mandates: {
+                    links: {
+                        customer: customerId
+                    }
+                }
+            });
+        nock('https://api-sandbox.gocardless.com')
+            .get('/customers/' + customerId)
+            .reply(200, {
+                customers: {
+                    email: "tim@gocardless.com"
+                }
+            });
+    });
 
+    it('should return a 204', function () {
         chai.request(app)
             .post('/updateMember')
             .send(webhook)
